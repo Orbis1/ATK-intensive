975e26a13de1746ddcb4ac81a32cedf71354e207
dvkrivosheev <dvkrivosheev@sberbank.ru>
2020-11-24 11:59:00 +0300

    refactoring

--- security/securityTools.js
+++ security/securityTools.js
@@ -1,106 +1,78 @@
 define(function () {
   return {
     removeExport: function () {
-      const removeExportNode = (contextMenu) => {
-        if (!contextMenu) return;
-
-        const isExportDataItem = (node) => {
-          let items = [];
-          // проверяем сам пункт меню
-          if (
-            node.hasAttribute("tid") &&
-            ["i9fd7", 'insight-chart-export-button'].includes(node.getAttribute("tid").toLowerCase())
-          ) {
-            items.push(node);
-          } else {
-            // проверяем содержимое пунка меню
-            node.childNodes.forEach((item) => {
-              if (
-                item instanceof HTMLElement &&
-                item.hasAttribute("tid") &&
-                [
-                  "export-group",
-                  "export",
-                  "cao-share",
-                  "79f6",
-                  "sharegroup",
-                ].includes(item.getAttribute("tid").toLowerCase())
-              ) {
-                items.push(item.parentElement);
-              }
-            });
-          }
-          console.log("isExportDataItem -> items", items);
-          return items.length > 0;
-        };
-
-        let itemsInMenu = 0;
-        let exportDataNode = [];
-
-        // перебираем все элементы меню
-        contextMenu.querySelectorAll("li, button").forEach((item) => {
-          if (isExportDataItem(item)) exportDataNode.push(item);
-          itemsInMenu++;
-        });
-
-        if (exportDataNode.length === 0) return;
-
-        // если в списке только один элемент, то удалить весь список, иначе только строку экспорта
-        if (itemsInMenu > exportDataNode.length) {
-          exportDataNode.forEach(item => item.style = "background-color: red");
-          // exportDataNode.remove();
-        } else {
-          contextMenu.style = "background-color: red";
-          // contextMenu.remove();
-        }
-      };
 
       // выбираем элемент
       let target = document.body;
-      let popover = null;
 
-      // создаем экземпляр наблюдателя, которые отлавливает появление контекстного меню
-      // filter of body`s mutations
-      let searchContextMenu = new MutationObserver((mutations) => {
-        mutations.forEach((mutation) => {
-          // console.log(mutation.target);
-          // console.log(mutation);
-          if (mutation.addedNodes.length > 0) {
-            mutation.addedNodes.forEach((node) => {
-              if (node instanceof HTMLElement && node.hasAttribute("tid")) {
-                popover = mutation.target;
-                watchContextMenu.observe(mutation.target, config);
-                removeExportNode(popover);
-              }
-              // для vizlib
-              if (
-                node instanceof HTMLElement &&
-                node.className.match("vzl-*")
-              ) {
-                node.querySelectorAll(".fa-file-excel-o").forEach((item) => {
-                  if (item.parentNode) item.parentNode.remove();
-                });
-              }
-            });
-          }
+      //#region Дополнительные функции
+
+      // возвращает элемент DOM соответствующий контекстному меню
+      const getPopover = (node) => {
+        const popoverIn = node.querySelector('.lui-popover');
+        const popoverOut = node.closest('.lui-popover');
+        return popoverIn || popoverOut;
+      };
+
+      // перебор всех дочерних элементо в node
+      const inspectChildNodes = (node) => {
+        let result = false;
+        if(node.childElementCount === 0) return;
+        node.childNodes.forEach(child => {
+          if(!(child instanceof HTMLElement)) return; // если child текст
+          if(result) return; // если предыдущий child isExportNode = true
+          if(isExportNode(child)) {
+            result = true
+          } else {
+            result = inspectChildNodes(child);
+          };
         });
-      });
+        return result;
+      };
 
-      // создаем экземпляр наблюдателя для изменений в контекстном меню
-      let watchContextMenu = new MutationObserver((mutations) => {
+      // возвращает множество элементов содержащиз экспорт
+      const getExportItems = (contextMenu) => {
+        let exportItems = new Set();
+        contextMenu.querySelectorAll('li').forEach(item => {
+          if(isExportNode(item)) exportItems.add(item);
+          if(inspectChildNodes(item)) exportItems.add(item);
+        });
+        return exportItems;
+      };
+
+      // определяет содержит ли в себе нода признаки экпорта
+      const isExportNode = (node) => {
+
+        // проверка по tid
+        if(node.hasAttribute("tid")) {
+          const tid = node.getAttribute("tid").toLowerCase();
+          return ["i9fd7", 'insight-chart-export-button',"export-group", "export", "cao-share", "79f6", "sharegroup"].includes(tid);
+        };
+
+        return false;
+      };
+
+      // Удаление элементов из DOM
+      const removeDOM = (setItems) => {
+        for (let value of setItems) {
+          value.style = 'background-color: red';
+          value.remove();
+        };
+      }
+      //#endregion
+
+      // создаем экземпляр наблюдателя, который отлавливает изменения в DOM
+      let searchDOMChanges = new MutationObserver((mutations) => {
         mutations.forEach((mutation) => {
-          // поиск добавления пунка в меню
-          if (mutation.addedNodes.length > 0) {
-            mutation.addedNodes.forEach((node) => {
-              if (
-                node instanceof HTMLElement &&
-                node.tagName === "LI" &&
-                node.querySelectorAll("span")[0].hasAttribute("tid")
-              ) {
-                removeExportNode(popover);
-              }
-            });
-          }
+          // console.log('mutation', mutation);
+          mutation.addedNodes.forEach((node) => {
+            if (!(node instanceof HTMLElement)) return;
+            const popover = getPopover(node);
+            if(popover) {
+              const exportItems = getExportItems(popover);
+              if(exportItems.size>0) removeDOM(exportItems);
+            }
+          });
         });
       });
 
@@ -113,7 +85,7 @@ define(function () {
       };
 
       // передаем элемент и настройки в наблюдатель
-      searchContextMenu.observe(target, config);
+      searchDOMChanges.observe(target, config);
 
       // позже можно остановить наблюдение
       // observer.disconnect();
